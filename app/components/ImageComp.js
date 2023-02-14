a02e3d60825f1a9b7d305541e3b610f346c7abd1
Shafique Ahmed <shafiqued17z@gmail.com>
2023-02-14 18:27:24 +0500

    Last edit before going to Hostel: Using state via useState() hook

--- app/components/ImageComp.js
+++ app/components/ImageComp.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Image, StyleSheet } from "react-native";
 
-function ImageComp(props) {
+function ImageComp({ imgAddress }) {
+const [showModal, setShowModal] = useState(false)
+
   return (
-      <Image
-        style={styles.image}
-        source={require("../assets/purple_flower.jpg")}
-      />
+    <Image
+      style={styles.image}
+      // resizeMode="contain"
+      source={require("../assets/purple_flower.jpg")}
+    />
   );
-};
+}
 
 const styles = StyleSheet.create({
-  image: { width: "90%", height: "75%", alignItems: "center", justifyContent: "center" },
+  image: {
+    width: "90%",
+    height: "75%",
+    alignItems: "center",
+    borderRadius: 15,
+    justifyContent: "center",
+  },
 });
 
 export default ImageComp;
